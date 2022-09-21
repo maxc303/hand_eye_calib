84d1267c1d09f0adced21320fc2b8f853eac5db0
maxc303 <ymchen303@hotmail.com>
2022-09-20 22:21:22 -0400

    Finish initial test

--- axxb_solver/src/main.cpp
+++ axxb_solver/src/main.cpp
@@ -18,23 +18,34 @@ int main()
 
     // Ground truth
     Eigen::Matrix4d T_a_b;
-    T_a_b << 1 / sqrt(2), -1 / sqrt(2), 0, 0,
-        1 / sqrt(2), 1 / sqrt(2), 0, 0,
-        0, 0, 1, 0,
+    // T_a_b << 1 / sqrt(2), -1 / sqrt(2), 0, 1,
+    //     1 / sqrt(2), 1 / sqrt(2), 0, 1,
+    //     0, 0, 1, 1,
+    //     0, 0, 0, 1;
+
+    T_a_b << 0.7071068, -0.7071068, 0.0000000, 1,
+        0.7044160, 0.7044160, -0.0871557, 1,
+        0.0616284, 0.0616284, 0.9961947, 1,
         0, 0, 0, 1;
 
     Eigen::Matrix4d B_last = A_last * T_a_b;
-    std::cout << B_last << std::endl;
     for (int i = 0; i < 10; i++)
     {
         Eigen::Matrix4d T_a_i = Eigen::Matrix4d::Zero();
-        T_a_i.block<3, 3>(0, 0) = Eigen::AngleAxisd(i/10.0 * M_PI, Eigen::Vector3d::UnitZ()).matrix();
+        T_a_i.block<3, 3>(0, 0) = Eigen::AngleAxisd((i + 1) / 30.0 * M_PI, Eigen::Vector3d::UnitZ()).matrix() * Eigen::AngleAxisd((i + 1) / 20.0 * M_PI, Eigen::Vector3d::UnitY()).matrix();
+
+        T_a_i(0, 3) = 1;
+        T_a_i(1, 3) = 1;
+        T_a_i(2, 3) = 1;
+        T_a_i(3, 3) = 1;
 
         Eigen::Matrix4d A_i = A_last * T_a_i;
         Eigen::Matrix4d B_i = A_i * T_a_b;
 
-        std::cout << "T_a_i " << std::endl << T_a_i << std::endl;
-        std::cout << "T_b_i " << std::endl << inverse(B_last) * B_i << std::endl;
+        // std::cout << "T_a_i " << std::endl
+        //           << T_a_i << std::endl;
+        // std::cout << "T_b_i " << std::endl
+        //           << inverse(B_last) * B_i << std::endl;
 
         T_a.push_back(T_a_i);
         T_b.push_back(inverse(B_last) * B_i);
--- axxb_solver/src/solver.hpp
+++ axxb_solver/src/solver.hpp
@@ -22,15 +22,21 @@ Eigen::Matrix3d solve_r(const std::vector<Eigen::Matrix4d> &T_a, const std::vect
         Eigen::Vector3d k_r_a = vee(r_a.log());
         Eigen::Vector3d k_r_b = vee(r_b.log());
 
-        v.block<3, 1>(3 * i, 0) = k_r_a - k_r_b;
+        k_r_a = k_r_a / k_r_a.norm();
+        k_r_b = k_r_b / k_r_b.norm();
+
         S.block<3, 3>(3 * i, 0) = skew(k_r_a + k_r_b);
+        v.block<3, 1>(3 * i, 0) = k_r_a - k_r_b;
     }
 
     std::cout << S << std::endl;
     std::cout << v << std::endl;
 
     Eigen::Vector3d k_r_prime = S.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(v);
-    std::cout << k_r_prime << std::endl;
-    std::cout << skew(k_r_prime).exp() << std::endl;
-    return skew(k_r_prime).exp();
+
+    double theta = 2 * std::atan(k_r_prime.norm());
+
+    Eigen::Vector3d k_r_axis = k_r_prime / k_r_prime.norm();
+    // std::cout << (Eigen::Matrix3d::Identity() * std::cos(theta) + std::sin(theta) * skew(k_r_axis) + (1 - std::cos(theta)) * k_r_axis * k_r_axis.transpose()).transpose() << std::endl;
+    return (skew(k_r_axis) * theta).exp().transpose();
 }
